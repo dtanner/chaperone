11780b3027b28a20b73f8866b9cc7b4c2c61683a
tanner <dan.tanner@target.com>
2020-02-10 13:28:58 -0600

    remove commented todos

--- src/main/kotlin/chaperone/ConfigLoader.kt
+++ src/main/kotlin/chaperone/ConfigLoader.kt
@@ -19,23 +19,6 @@ data class OutputConfig(
     val uri: String? = null
 )
 
-// unused...see if this is useful for validating and getting a focused output config
-sealed class OutputWriterConfig {
-    object StdOut : OutputWriterConfig()
-    data class InfluxDb(val uri: String, val tags: Map<String, String>) : OutputWriterConfig()
-}
-
-//enum class OutputConfigType(outputWriterImplementation: Class<out OutputWriter>) {
-//    stdout(StdOutWriter::class.java),
-//    influxdb(InfluxDbWriter::class.java)
-//}
-
-// todo figure out sealed class loading for konf, or conditionally non-null properties,
-//  or a custom translator to go from big generic to output-specific config
-
-
-// switch to json config format?
-// allow users to choose?
 fun loadConfig(configFile: File): AppConfig {
     return Config()
         .from.toml.file(configFile)
